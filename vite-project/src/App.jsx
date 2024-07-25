d6ca9aea0be912a3bf214bf3e967366d442539e5
Pósán Róbert <posanrobi@gmail.com>
2024-07-25 22:39:28 +0200

    del add done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo } from "./store/store";
+import { addTodo, deleteTodo, toggleTodo } from "./store/store";
+import classes from "./Todos.module.css";
 
 function App() {
   const [text, setText] = useState("");
@@ -13,6 +14,14 @@ function App() {
       dispatch(addTodo(text));
       setText("");
     }
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch(toggleTodo(id));
   };
 
   const handleInputChange = (e) => {
@@ -27,11 +36,50 @@ function App() {
           value={text}
           onChange={handleInputChange}
           placeholder="Add todo"
+          className={classes.inputField}
         />
         <button onClick={handleAddTodo}>Add</button>
+        <ul>
+          {todos.map((todo) => {
+            const date = new Date(todo.id);
+            const formattedDate = date.toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            });
+
+            const itemClass = todo.completed
+              ? `${classes.todoItem} ${classes.completedTodo}`
+              : classes.todoItem;
+
+            return (
+              <li
+                key={todo.id}
+                className={`${itemClass} ${classes.todoItemText}`}
+              >
+                <div>
+                  {`${formattedDate} - ${todo.text} ${
+                    todo.completed ? "Completed" : "Not completed"
+                  }`}
+                </div>
+                <div className={classes.todoItemButtons}>
+                  <button onClick={() => handleToggleTodo(todo.id)}>{`${
+                    todo.completed ? "Cancel" : "Done"
+                  }`}</button>
+                  <button
+                    onClick={() => handleDeleteTodo(todo.id)}
+                    className={classes.deleteTodo}
+                  >
+                    Del
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+          {todos.length === 0 && <p>No todos yet</p>}
+        </ul>
       </div>
     </>
   );
 }
-
 export default App;
